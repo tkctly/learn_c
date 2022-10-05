fe37b999da7c779e4192ab377c828ed7717551ea
tkctly <thinkerception@gmail.com>
2022-10-05 17:50:41 +0800

    feat(part_6): pratice3.c

---
+++ part_6/pratice3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int x = 0;
+    x > 5;
+    scanf("%lf", &x) != false;
+    x == 5;
+    scanf("%d", &x);
+    x != 5;
+    x >= 20;
+}
