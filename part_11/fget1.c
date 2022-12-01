28e24b98c4099261bcfe6f914252b3e644a702ab
tkctly <thinkerception@gmail.com>
2022-12-01 16:03:42 +0800

    feat(part_11): fget1.c

---
+++ part_11/fget1.c
@@ -0,0 +1,21 @@
+/* fgets1.c -- 使用 fgets() fputs() */
+#include <stdio.h>
+#define STLEN 14
+int main(void)
+{
+    char words[STLEN];
+
+    puts("Enter a string, please.");
+    fgets(words, STLEN, stdin);
+    printf("Your string twice (puts(), then fput()):\n");
+    puts(words);
+    fputs(words, stdout);
+    puts("enter another string, please.");
+    fgets(words, STLEN, stdin);
+    printf("Your string twice (puts(), then fputs()):\n");
+    puts(words);
+    fputs(words, stdout);
+    puts("Done.");
+
+    return 0;
+}
