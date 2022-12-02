cb3e43aa4cdfe39f723b35488b2bf1d078bb51a9
tkctly <thinkerception@gmail.com>
2022-12-02 12:01:50 +0800

    feat(part_11): nono.c

---
+++ part_11/nono.c
@@ -0,0 +1,12 @@
+/* nono.c -- 千万不要模仿！ */
+#include <stdio.h>
+int main(void)
+{
+    char side_1[] = "Side A";
+    char dont[] = {'W', 'O', 'W', '!'};
+    char side_b[] = "Side B";
+
+    puts(dont);
+
+    return 0;
+}
